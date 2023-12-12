a7e4aafa4589532acc73284afb2fadda182953ed
darshit120903 <darshit.web.moweb@gmail.com>
2023-12-12 10:21:58 +0530

    react-task

--- src/Components/Basic.js
+++ src/Components/Basic.js
@@ -1,21 +1,60 @@
-import React, { useState } from "react";
-function Basic({ names, token }) {
-  const sendMail = async () => {
-    const api =
-      "https://eaf-dms-api.yecor.com/api/inventory/bulk_stock_in_out/";
+import React, { useState, useEffect } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
 
-    const isMissingData = containers.some(
-      (container) =>
-        !container.selectedValue ||
-        !container.batchNumber ||
-        !container.updatedDate ||
-        !container.inwardQty
-    );
+function Basic({ names, token, loadMore, hasMore ,fetchInventoryData,search,page ,setInventory,setPage}) {
+  
+  const [containers, setContainers] = useState([
+    { selectedValue: "", batchNumber: "", updatedDate: "", inwardQty: "" },
+  ]);
+  const [options, setOptions] = useState([])
+  const [missingFields, setMissingFields] = useState([])
+
+  useEffect(() => {
+    setOptions([]);
+    const uniqueOptions = names.map((product) => ({
+      value: product.id,
+      label: product.name,
+    }))
+
+    setOptions((prevOptions) => {
+      const stringifiedPrevOptions = prevOptions.map(JSON.stringify);
+      const stringifiedUniqueOptions = uniqueOptions.map(JSON.stringify);
+      const combinedStrings = [
+        ...new Set([...stringifiedPrevOptions, ...stringifiedUniqueOptions]),
+      ];  
+      return combinedStrings.map(JSON.parse);
+    })
+  },[names,setInventory,page,loadMore])
+
+  const sendMail = async () => {
+    const api = "https://eaf-dms-api.yecor.com/api/inventory/bulk_stock_in_out/";
+    const missingFieldsArray = containers.map((container, index) => {
+      const missingFields = [];
+      
+      if (!container.selectedValue) {
+        missingFields.push("Product SKU");
+      }
+      if (!container.batchNumber) {
+        missingFields.push("Batch Number");
+      }
+      if (!container.updatedDate) {
+        missingFields.push("Expiry Date");
+      }
+      if (!container.inwardQty) {
+        missingFields.push("Inward Qty");
+      }
+  
+      return { index, missingFields };
+    });
+  
+    const isMissingData = missingFieldsArray.some((fields) => fields.missingFields.length > 0);
+  
     if (isMissingData) {
-      alert(
-        "Please fill in all the fields for each container before submitting."
-      );
-    } else {
+      setMissingFields(missingFieldsArray);
+      console.log("this is missing data:",missingFields)
+      return;
+    } 
+    else {
       const request = {
         stock_entries: containers.map((container) => ({
           product_id: `${container.selectedValue}`,
@@ -38,151 +77,177 @@ function Basic({ names, token }) {
         });
         if (response) {
           const data = await response.json();
-          console.log(data);
+          console.log(data)
           Cancel()
         } else {
-          console.log("api failed:", response.statusText);
+          console.log("api failed:", response.statusText)
         }
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     }
   };
-  const [containers, setContainers] = useState([
-    { selectedValue: "", batchNumber: "", updatedDate: "", inwardQty: "" },
-  ]);
-  //console.log("this is container", containers)
-  const handleSelectChange = (e, index) => {
-    const updatedContainers = [...containers];
-    updatedContainers[index].selectedValue = e.target.value;
-    setContainers(updatedContainers);
-  };
+  console.log("this is missing field:",missingFields)
+  const handleSelectChange = (selectedOption, index) => {
+    const updatedContainers = [...containers]
+    updatedContainers[index].selectedValue = selectedOption.value
+    setContainers(updatedContainers)
+  }
+
   const handleBatchNumberChange = (e, index) => {
-    const regrex =/^[0-9#]*$/
-    const inputValue = e.target.value;
-    console.log(inputValue);
-    if (regrex.test(inputValue) || inputValue==="") {
+    const regrex = /^[0-9#]*$/;
+    const inputValue = e.target.value
+    if (regrex.test(inputValue) || inputValue === "") {
       const updatedContainers = [...containers];
       updatedContainers[index].batchNumber =
-        inputValue[0] === "#" ? inputValue :"#"+inputValue
-      setContainers(updatedContainers);
+        inputValue[0] === "#" ? inputValue : "#" + inputValue;
+      setContainers(updatedContainers)
     } else {
-      alert("Batch Number should contain only numeric values.");
-      return;
+      alert("Batch Number should contain only numeric values.")
+      return
     }
-  };  
-  const handleAddContainer = () => {
+  }
+
+  
+  const handleAddContainer = async() => {
     setContainers([
       ...containers,
       { selectedValue: "", batchNumber: "", updatedDate: "", inwardQty: "" },
-    ]);
-  };
+    ])   
+    setPage(1)
+    const pageSize = 10
+    const offset = 0
+    const inventoryApiUrl = `https://eaf-dms-api.yecor.com/api/inventory/product-SKUs/?warehouse_id=22&ordering=name&search=${search}&limit=${pageSize}&offset=${offset}&remove_product_stocking=true`;
+    await fetchInventoryData(inventoryApiUrl, token, setInventory,hasMore)
+    setOptions([])
+    loadMore()
+  }
+
   const handleRemoveContainer = (index) => {
     if (containers.length > 1) {
-      const updatedContainers = [...containers];
-      updatedContainers.splice(index, 1);
-      setContainers(updatedContainers);
+      const updatedContainers = [...containers]
+      updatedContainers.splice(index, 1)
+      setContainers(updatedContainers)
     } else {
       alert("you cannot delete the container");
     }
-  };
+  }
+
   const handleDate = (e, index) => {
-    const updatedContainers = [...containers];
-    updatedContainers[index].updatedDate = e.target.value;
-    setContainers(updatedContainers);
-  };
+    const updatedContainers = [...containers]
+    updatedContainers[index].updatedDate = e.target.value
+    setContainers(updatedContainers)
+  }
+
   const handleInwardQty = (e, index) => {
-    const quantity = e.target.value
-    const Re = /^[0-9]+(\.[0-9]+)?$/
-      if(Re.test(quantity) || quantity===""){
-        const updatedContainers = [...containers];
-    updatedContainers[index].inwardQty =quantity;
-    setContainers(updatedContainers);
-      }
-      else{
-        alert("enter only number")
-      }
-  };
+    const quantity = e.target.value;
+    const Re = /^[0-9]+(\.[0-9]+)?$/;
+    if (Re.test(quantity) || quantity === "") {
+      const updatedContainers = [...containers];
+      updatedContainers[index].inwardQty = quantity;
+      setContainers(updatedContainers);
+    } else {
+      alert("enter only number");
+    }
+  }
+
   const Cancel = () => {
     setContainers([
       { selectedValue: "", batchNumber: "", updatedDate: "", inwardQty: "" },
-    ]);
-  };
+    ])
+    setMissingFields([])
+  }
+
+  const loadOptions = async (search) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  loadMore();
+  if (!search) {
+    return {
+      options,
+      hasMore,
+    }
+  } else {
+    const searchLower = search.toLowercase()
+    const filteredOptions = options.filter(({ label }) =>
+      label.toLowercase().includes(searchLower)
+    )
+    return {
+      options:filteredOptions,
+      hasMore,
+    }
+  }
+}
   return (
     <div className="border border-success">
       <br />
-    
-{containers.map((container, index) => (
-  <div key={index} className="container">
-    <div className="row align-items-center">
-      <div className="col-md-3">
-        <h6>Product SKU</h6>
-        <div className="form-group">
-          <select
-            className="form-control"
-            value={container.selectedValue}
-            onChange={(e) => handleSelectChange(e, index)}
-          >
-            <option value="">--Select--</option>
-            {names.map((product, productIndex) => (
-              <option key={productIndex} value={product.id}>
-                {product.name}
-              </option>
-            ))}
-          </select>
+      {containers.map((container, index) => (
+        <div key={index} className="container">
+          <div className="row align-items-center">
+            <div className="col-md-3">
+              <h6>Product SKU</h6>
+              <div className="form-group">
+                <AsyncPaginate
+                className={` ${missingFields[index]?.missingFields.includes('Product SKU') ? 'border border-danger' : ''}`}
+                isSearchable
+                  value={containers.selectedValue}
+                  onChange={(selectedOption) =>
+                    handleSelectChange(selectedOption, index)
+                  }
+                loadOptions={(search)=>loadOptions(search)}
+                />               
+              </div>
+            </div>
+            <div className="col-md-3">
+              <h6>Batch Number</h6>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className={`form-control ${missingFields[index]?.missingFields.includes('Batch Number') ? 'border border-danger' : ''}`}
+                  placeholder="Type here..."
+                  value={container.batchNumber}
+                  onChange={(e) => handleBatchNumberChange(e, index)}
+                />
+              </div>
+            </div>
+            <div className="col-md-3">
+              <h6>Expiry Date</h6>
+              <div className="form-group">
+                <input
+                  type="date"
+                  className={`form-control  ${missingFields[index]?.missingFields.includes('Expiry Date') ? 'border border-danger' : ''}`}
+                  value={
+                    container.updatedDate === ""
+                      ? new Date().toISOString().split("T")[0]
+                      : container.updatedDate
+                  }
+                  onChange={(e) => handleDate(e, index)}
+                  min={new Date().toISOString().split("T")[0]}
+                />
+              </div>
+            </div>
+            <div className="col-md-2">
+              <h6>Inward Qty</h6>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className={`form-control ${missingFields[index]?.missingFields.includes('Inward Qty') ? 'border border-danger' : ''}`}
+                  placeholder="Type Here..."
+                  value={container.inwardQty}
+                  onChange={(e) => handleInwardQty(e, index)}
+                />
+              </div>
+            </div>
+            <div className="col-md-1">
+              <button
+                className="btn btn-danger"
+                onClick={() => handleRemoveContainer(index)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="col-md-3">
-        <h6>Batch Number</h6>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Type here..."
-            value={container.batchNumber}
-            onChange={(e) => handleBatchNumberChange(e, index)}
-          />
-        </div>
-      </div>
-      <div className="col-md-3">
-        <h6>Expiry Date</h6>
-        <div className="form-group">
-          <input
-            type="date"
-            className="form-control"
-            value={
-              container.updatedDate === ""
-                ? new Date().toISOString().split("T")[0]
-                : container.updatedDate
-            }
-            onChange={(e) => handleDate(e, index)}
-            min={new Date().toISOString().split("T")[0]}
-          />
-        </div>
-      </div>
-      <div className="col-md-2">
-        <h6>Inward Qty</h6>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Type Here..."
-            value={container.inwardQty}
-            onChange={(e) => handleInwardQty(e, index)}
-          />
-        </div>
-      </div>
-      <div className="col-md-1">
-        <button
-          className="btn btn-danger"
-          onClick={() => handleRemoveContainer(index)}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
-  </div>
-))}
+      ))}
       <br />
       <div>
         <button className="btn btn-secondary" onClick={handleAddContainer}>
@@ -206,6 +271,6 @@ function Basic({ names, token }) {
       </div>
       <br />
     </div>
-  );
+  )
 }
-export default Basic;
+export default Basic
--- src/stock.js
+++ src/stock.js
@@ -4,36 +4,29 @@ import Basic from "./Components/Basic"
 function Stock() {
     const [token, setToken] = useState('');
     const [inventory, setInventory] = useState([])
-
+    const [page, setPage] = useState(1); 
+    const [search, setSearch] = useState('')
+    const [hasMore,setHasMore] = useState(true)
+   
     useEffect(() => {
-        fetchData();
-    }, [])
-
-    useEffect(() => {
-        if (token) {
-            const inventoryApiUrl = 'https://eaf-dms-api.yecor.com/api/inventory/product-SKUs/?warehouse_id=22&ordering=name&search=&limit=1000&offset=&remove_product_stocking=true';
-            const fetchInventoryData = async () => {
-                try {
-                    const response = await fetch(inventoryApiUrl, {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    });
-                    if (response.ok) {
-                        const inventoryData = await response.json();
-                        console.log('Inventory API response:', inventoryData);
-                        //console.log("this is inventory data:", inventoryData.results)
-                        setInventory(inventoryData.results)
-                    } else {
-                        console.error('Inventory API request failed:', response.statusText);
-                    }
-                } catch (error) {
-                    console.error('Error:', error);
-                }
-            };
-            fetchInventoryData();
+        const storedToken= localStorage.getItem(token)
+        console.log("this is storedtoken:",storedToken)
+        if(!storedToken){
+            fetchData()
         }
-    },[token]);
+        else{
+            setToken(storedToken)
+        }
+    },[])
+
+    useEffect(() => {  
+        if (token && hasMore) {           
+            const pageSize = 10; 
+            const offset = (page-1) * pageSize;
+            const inventoryApiUrl = `https://eaf-dms-api.yecor.com/api/inventory/product-SKUs/?warehouse_id=22&ordering=name&search=${search}&limit=${pageSize}&offset=${offset}&remove_product_stocking=true`
+           fetchInventoryData(inventoryApiUrl, token, setInventory, setHasMore)           
+        }
+    },[page,search,hasMore,token]);
     const fetchData = async () => {
         const apiUrl = 'https://eaf-dms-api.yecor.com/api/auth/login'
         const requestData = {
@@ -53,22 +46,53 @@ function Stock() {
             });
             if (response.ok) {
                 const data = await response.json();
+                console.log(data)
+                console.log(data.token)
+                setToken("")
                 setToken(data.token)
-                console.log('API response:', data);
+                localStorage.setItem('token',data.token)
+                console.log('API response:', data)
 
             } else {
-                console.error('API request failed:', response.statusText);
+                console.error('API request failed:', response.statusText)
             }
         } catch (error) {
-            console.error('Error:', error);
+            console.log("hello1")
+            console.log("this is error code",error.code)
+            console.log("hello2")
+            console.error('Error:', error)
         }
     };
+     
+    const fetchInventoryData = async (inventoryApiUrl, token, setInventory, setHasMore) => {
+        try {  
+            const response = await fetch(inventoryApiUrl, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            if (response.ok) {
+                const inventoryData = await response.json()                  
+                setInventory(inventoryData.results)            
+                                        
+                console.log("this is null::",inventoryData.next)
+            } else {
+                console.error('Inventory API request failed:', response.statusText)
+            }
+        } catch (error) {
+            console.error('Error:', error)
+        }
+    }
+    const handleLoadMore = () => {
+        setPage((prevPage) => prevPage + 1)
+      }
+   
     return (
         <div className="text-center">
             <h1>Stock</h1>
             <br></br>
-            <Basic names={inventory} token={token}/>
+            <Basic names={inventory} token={token} loadMore={handleLoadMore} hasMore={hasMore} page={page} setHasMore={setHasMore} setPage={setPage} fetchInventoryData={fetchInventoryData} search={search} setInventory={setInventory}/>
         </div>
     )
 }
-export default Stock
+export default Stock 
